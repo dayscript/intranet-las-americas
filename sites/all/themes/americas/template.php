1ea7e17d339bd2534fa0b8fb9283a39b06b270a7
jcardenas2015 <jcardenas@dayscript.com>
2017-10-25 11:17:56 -0500

    git inicial

---
+++ sites/all/themes/americas/template.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Implements template_preprocess_html().
+ */
+function americas_preprocess_html(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_page.
+ */
+function americas_preprocess_page(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_node.
+ */
+function americas_preprocess_node(&$variables) {
+}
